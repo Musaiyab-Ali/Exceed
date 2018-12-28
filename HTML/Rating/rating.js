fd67d52c0ca9f545a58240a6c17845ab1e541230
Ralph Huang <rhuang5885@gmail.com>
2018-12-28 18:57:29 -0500

    Doughnut chart

--- HTML/Rating/rating.js
+++ HTML/Rating/rating.js
@@ -1,69 +1,51 @@
 let total = Number(0);
 let count = 0;
 
-var map1 = new Map([ ["str1", 0],["str2", 0],["str3", 0],["str4", 0]["str5", 0]]);
-
-let ctx = document.getElementById('myChart');
+let map1 = {
+    "str5" : 20,
+    "str4" : 24,
+    "str3" : 33,
+    "str2" : 13,
+    "str1" : 32,
+};
 
 function addFive(){
     total += 5;
     count += 1;
     map1["str5"] += 1;
-    document.getElementById("test").innerHTML = average();
+    document.getElementById('average').innerHTML = "Average: " + average();
 }
 
 function addFour(){
     total += 4;
     count += 1;
     map1["str4"] += 1;
-    document.getElementById('test').innerHTML = average();
+    document.getElementById('average').innerHTML = "Average: " + average();
 }
 
 function addThree(){
     total += 3;
     count += 1;
     map1["str3"] += 1;
-    document.getElementById("test").innerHTML = average();
+    document.getElementById('average').innerHTML = "Average: " + average();
 }
 
 function addTwo(){
     total += 2;
     count += 1;
     map1["str2"] += 1;
-    document.getElementById("test").innerHTML = average();
+    document.getElementById('average').innerHTML = "Average: " + average();
 }
 
 function addOne(){
     total += 1;
     count += 1;
     map1["str1"] += 1;
-    document.getElementById("test").innerHTML = average();
+    document.getElementById('average').innerHTML = "Average: " + average();
 }
 
 function average(){
     let average = total / count;
-    document.getElementById('average').innerHTML = "Average: " + average;
+    return average.toFixed(2);
 }
 
-
-
-data = {
-    datasets: [{
-        data: [2,3,5,2,3]
-    }],
-    labels: [
-        'Red',
-        'Orange',
-        'Green',
-        'Blue',
-        'Yellow'
-    ]
-};
-
-let myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
-});
-
-console.log(map1);
